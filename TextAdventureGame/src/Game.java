c5a9cbbab4a705f97d3115c8d68acfc033174b25
BrianKuiters <152171842+BrianKuiters@users.noreply.github.com>
2025-03-07 17:33:57 +0100

    Made a titlescreen

--- TextAdventureGame/src/Game.java
+++ TextAdventureGame/src/Game.java
@@ -1,11 +1,17 @@
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Game {
 
     JFrame window;
     Container con;
+    JPanel titleNamePanel;
+    JLabel titleText;
+    Font titleFont = new Font("Gothic", Font.PLAIN, 90);
 
     public static void main(String[] args) {
 
@@ -22,6 +28,18 @@ public class Game {
         window.setLayout(null);
         window.setVisible(true); //Zonder dit zie je de window niet
         this.con = window.getContentPane();
+        //Titel van het spel
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100,100,600,150);
+        titleNamePanel.setBackground(Color.blue);
+        titleText = new JLabel("ADVENTURE");
+        titleText.setForeground(Color.white);
+        titleText.setFont(titleFont);
+        titleNamePanel.add(titleText);
+        con.add(titleNamePanel);
+
+        titleNamePanel.setVisible(true);
+        titleText.setVisible(true);
 
     }
 
