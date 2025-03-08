1be110df049076aacb16341a22bb85b4acf77de1
BrianKuiters <152171842+BrianKuiters@users.noreply.github.com>
2025-03-08 15:18:22 +0100

    Added buttons, start of GUI, maintextpanels, buttonpanels, buttons, and text.

--- TextAdventureGame/src/Game.java
+++ TextAdventureGame/src/Game.java
@@ -1,19 +1,26 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Game {
 
     JFrame window;
     Container con;
-    JPanel titleNamePanel, startButtonPanel;
+    JPanel titleNamePanel, startButtonPanel, mainTextPanel, choicebtnPanel;
     JLabel titleText;
-    JButton startButton;
+    JButton startButton,choice1,choice2,choice3,choice4;
+    JTextArea mainTextArea;
+
+    TitleScreenHandler tsHandler = new TitleScreenHandler();
 
     Font titleFont = new Font("Gothic", Font.PLAIN, 90);
     Font btnFont = new Font("Gothic", Font.PLAIN, 30);
@@ -50,6 +57,7 @@ public class Game {
         startButton.setBackground(Color.black);
         startButton.setForeground(Color.white);
         startButton.setFont(btnFont);
+        startButton.addActionListener(tsHandler);
         
         titleNamePanel.add(titleText);
         startButtonPanel.add(startButton);
@@ -65,6 +73,64 @@ public class Game {
         
         
 
+    }
+
+    public void createGameScreen(){
+        titleNamePanel.setVisible(false);
+        startButtonPanel.setVisible(false);
+
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100,100,600,250);
+        mainTextPanel.setBackground(Color.black);
+        con.add(mainTextPanel);
+
+            mainTextArea = new JTextArea("This is the main text area");
+            mainTextArea.setBounds(100,100,600,250);
+            mainTextArea.setBackground(Color.black);
+            mainTextArea.setForeground(Color.white);
+            mainTextArea.setFont(normalFont);
+            mainTextArea.setLineWrap(true);
+            mainTextPanel.add(mainTextArea);
+
+        choicebtnPanel = new JPanel();
+        choicebtnPanel.setBounds(250,350,300,150);
+        choicebtnPanel.setBackground(Color.black);
+        choicebtnPanel.setLayout(new GridLayout(4,1));
+        con.add(choicebtnPanel);
+
+            choice1 = new JButton("Choice 1");
+            choice1.setBackground(Color.black);
+            choice1.setForeground(Color.white);
+            choice1.setFont(normalFont);
+            choicebtnPanel.add(choice1);
+
+            choice2 = new JButton("Choice 2");
+            choice2.setBackground(Color.black);
+            choice2.setForeground(Color.white);
+            choice2.setFont(normalFont);
+            choicebtnPanel.add(choice2);
+
+            choice3 = new JButton("Choice 3");
+            choice3.setBackground(Color.black);
+            choice3.setForeground(Color.white);
+            choice3.setFont(normalFont);
+            choicebtnPanel.add(choice3);
+
+            choice4 = new JButton("Choice 4");
+            choice4.setBackground(Color.black);
+            choice4.setForeground(Color.white);
+            choice4.setFont(normalFont);
+            choicebtnPanel.add(choice4);
+        
+    }
+
+    public class TitleScreenHandler implements ActionListener{
+
+        public void actionPerformed(ActionEvent event){
+            //Remove titlescreen
+            createGameScreen();
+
+        }
     }
 
 }
